c8264950152d4aca35212e00508d1074dfc83e37
hnviqz <446591248@qq.com>
2016-10-11 17:43:57 +0800

    添加reverse方法的注释

--- src/cn/longfox/tutorialspoint/java/example/Strings/ReverseString.java
+++ src/cn/longfox/tutorialspoint/java/example/Strings/ReverseString.java
@@ -1,5 +1,16 @@
 package cn.longfox.tutorialspoint.java.example.Strings;
 
+/**
+ * 
+ * @author Administrator
+ *	功能：通过reverse方法倒置一个给定的字符串：如
+ *  字符串：“abcde”经过倒置后变成：“edcba”
+ *
+ *
+ */
+
+
+
 public class ReverseString {
 	public static void main(String []args){
 		String str = "abcde";
