d76b0012f8232e64a8d556614e93d28410b71cfa
Administrator <Administrator@W012>
2016-10-12 11:06:06 +0800

    添加RegionMatches类的注释

--- src/cn/longfox/tutorialspoint/java/example/Strings/RegionMatches.java
+++ src/cn/longfox/tutorialspoint/java/example/Strings/RegionMatches.java
@@ -1,5 +1,11 @@
 package cn.longfox.tutorialspoint.java.example.Strings;
-
+/**
+ * RegionMatches.java
+ * 创建日期：2016-10-12
+ * 功能：测试两个字符串中的相同区域
+ * @author Administrator
+ *
+ */
 public class RegionMatches {
 	public static void main(String args[]){
 		String strFirst = "welcome to microsoft";
