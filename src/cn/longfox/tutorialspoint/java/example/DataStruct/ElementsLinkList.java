1813b201f2f8e019a28ce27c606eb2e03f5385e8
Administrator <Administrator@W012>
2016-10-13 16:35:31 +0800

    添加LindedList类的使用

---
+++ src/cn/longfox/tutorialspoint/java/example/DataStruct/ElementsLinkList.java
@@ -0,0 +1,19 @@
+package cn.longfox.tutorialspoint.java.example.DataStruct;
+import java.util.LinkedList;
+/**
+ * 实现一个整数类型的链表
+ * 使用的方法为：LinkList类add();
+ * @author Administrator
+ *
+ */
+public class ElementsLinkList {
+	public static void main(String args[]){
+		LinkedList Listdemo =  new LinkedList();
+		Listdemo.add("200");
+		Listdemo.add("300");
+		Listdemo.add("400");
+		Listdemo.add("500");
+		System.out.println("the first element is "+Listdemo.getFirst());
+		System.out.println("the last element is "+Listdemo.getLast());
+	}
+}
