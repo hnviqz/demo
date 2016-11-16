65601684e91a38a2da9c7a4b115a02a970e54b7d
Administrator <Administrator@W012>
2016-11-16 09:20:16 +0800

    review

--- src/cn/longfox/tutorialspoint/java/example/DataStruct/ElementsLinkList.java
+++ src/cn/longfox/tutorialspoint/java/example/DataStruct/ElementsLinkList.java
@@ -15,5 +15,6 @@ public class ElementsLinkList {
 		Listdemo.add("500");
 		System.out.println("the first element is "+Listdemo.getFirst());
 		System.out.println("the last element is "+Listdemo.getLast());
+		System.out.println("XXX");
 	}
 }
